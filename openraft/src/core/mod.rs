5fe1e6bf125b869dc6571ae89343b164c717365a
Yijun Zhao <ariesdevil77@gmail.com>
2022-01-04 15:36:04 +0800

    implicit drop replication_rx

--- openraft/src/core/mod.rs
+++ openraft/src/core/mod.rs
@@ -738,9 +738,9 @@ impl<'a, D: AppData, R: AppDataResponse, N: RaftNetwork<D>, S: RaftStorage<D, R>
         loop {
             if !self.core.target_state.is_leader() {
                 tracing::info!("id={} state becomes: {:?}", self.core.id, self.core.target_state);
-                // notify to all nodes DO NOT send replication event any more.
-                self.replication_rx.close();
 
+                // implicit drop replication_rx
+                // notify to all nodes DO NOT send replication event any more.
                 return Ok(());
             }
 
