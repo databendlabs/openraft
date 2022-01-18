6f1c36ac109d314435e6128c0b27a1d3e2fd2e71
张炎泼 <drdr.xp@gmail.com>
2022-01-18 17:53:53 +0800

    Fixup: add new test to main.rs

--- openraft/tests/snapshot/main.rs
+++ openraft/tests/snapshot/main.rs
@@ -7,3 +7,4 @@ mod snapshot_ge_half_threshold;
 mod snapshot_line_rate_to_snapshot;
 mod snapshot_overrides_membership;
 mod snapshot_uses_prev_snap_membership;
+mod after_snapshot_add_learner_and_request_a_log;
