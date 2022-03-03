f9af5c5994e0169a36b1434a4927e3511b285477
lichuang <lichuang1982@gmail.com>
2022-03-03 15:33:51 +0800

    Fix: merge from upstream/main

--- openraft/tests/append_entries/main.rs
+++ openraft/tests/append_entries/main.rs
@@ -5,9 +5,6 @@ mod fixtures;
 // The number indicate the preferred running order for these case.
 // The later tests may depend on the earlier ones.
 
-mod t60_large_heartbeat;
-
-/*
 mod t10_conflict_with_empty_entries;
 mod t20_append_conflicts;
 mod t30_append_inconsistent_log;
@@ -15,4 +12,3 @@ mod t40_append_updates_membership;
 mod t50_append_entries_with_bigger_term;
 mod t60_large_heartbeat;
 mod t90_issue_216_stale_last_log_id;
-*/
