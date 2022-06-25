979a4bc773af65452511851548ed701f7b97925e
张炎泼 <drdr.xp@gmail.com>
2022-06-25 23:59:41 +0800

    Refactor: allow building a Joint quorum set from slice or vec

--- openraft/src/quorum/joint.rs
+++ openraft/src/quorum/joint.rs
@@ -5,37 +5,55 @@ use crate::quorum::QuorumSet;
 /// Use another data as a joint quorum set.
 ///
 /// The ids has to be a quorum in every sub-config to constitute a joint-quorum.
-pub(crate) trait AsJoint<'d, ID, QS>
+pub(crate) trait AsJoint<'d, ID, QS, D>
 where
     ID: 'static,
     QS: QuorumSet<ID>,
 {
-    fn as_joint(&'d self) -> Joint<'d, ID, QS>;
+    fn as_joint(&'d self) -> Joint<ID, QS, D>
+    where D: 'd;
 }
 
 /// A wrapper that uses other data to define a joint quorum set.
 ///
 /// The input ids has to be a quorum in every sub-config to constitute a joint-quorum.
-pub(crate) struct Joint<'d, ID, QS>
+pub(crate) struct Joint<ID, QS, D>
 where
     ID: 'static,
     QS: QuorumSet<ID>,
 {
-    data: &'d [QS],
-    _p: PhantomData<ID>,
+    data: D,
+    _p: PhantomData<(ID, QS)>,
 }
 
-impl<'d, ID, QS> Joint<'d, ID, QS>
+impl<ID, QS, D> Joint<ID, QS, D>
 where
     ID: 'static,
     QS: QuorumSet<ID>,
 {
-    pub(crate) fn new(data: &'d [QS]) -> Self {
+    pub(crate) fn new(data: D) -> Self {
         Self { data, _p: PhantomData }
     }
 }
 
-impl<'d, ID, QS> QuorumSet<ID> for Joint<'d, ID, QS>
+/// Implement QuorumSet for Joint<..&[QS]>
+impl<'d, ID, QS> QuorumSet<ID> for Joint<ID, QS, &'d [QS]>
+where
+    ID: 'static,
+    QS: QuorumSet<ID>,
+{
+    fn is_quorum<'a, I: Iterator<Item = &'a ID> + Clone>(&self, ids: I) -> bool {
+        for child in self.data.iter() {
+            if !child.is_quorum(ids.clone()) {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+/// Implement QuorumSet for Joint<..Vec<QS>>
+impl<ID, QS> QuorumSet<ID> for Joint<ID, QS, Vec<QS>>
 where
     ID: 'static,
     QS: QuorumSet<ID>,
