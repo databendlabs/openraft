2e29b8e406509bf1cd3dbd33ba2af91ad681dac9
Heinz N. Gies <heinz@licenser.net>
2022-07-07 15:34:28 +0200

    Refactor: suite call all builders on a per test basis
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- memstore/src/test.rs
+++ memstore/src/test.rs
@@ -1,8 +1,27 @@
+use std::future::Future;
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use openraft::testing::StoreBuilder;
 use openraft::testing::Suite;
 use openraft::StorageError;
 
+use crate::Config;
 use crate::MemNodeId;
 use crate::MemStore;
+
+struct MemBuilder {}
+#[async_trait]
+impl StoreBuilder<Config, Arc<MemStore>> for MemBuilder {
+    async fn run_test<Fun, Ret, Res>(&self, t: Fun) -> Result<Ret, StorageError<MemNodeId>>
+    where
+        Res: Future<Output = Result<Ret, StorageError<MemNodeId>>> + Send,
+        Fun: Fn(Arc<MemStore>) -> Res + Sync + Send,
+    {
+        let store = MemStore::new_async().await;
+        t(store).await
+    }
+}
 
 /// To customize a builder:
 ///
@@ -25,8 +44,9 @@ use crate::MemStore;
 ///     Suite::test_all(MemStoreBuilder {})
 /// }
 /// ```
+
 #[test]
 pub fn test_mem_store() -> Result<(), StorageError<MemNodeId>> {
-    Suite::test_all(MemStore::new_async)?;
+    Suite::test_all(MemBuilder {})?;
     Ok(())
 }
