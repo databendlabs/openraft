6ff842f370e4335789506fd1b4d2c97b97de2193
张炎泼 <drdr.xp@gmail.com>
2022-07-10 17:40:30 +0800

    Refactor: fix test: after_snapshot_add_learner_and_request_a_log should expect snapshot at index 10, not 11

--- openraft/tests/snapshot/t40_after_snapshot_add_learner_and_request_a_log.rs
+++ openraft/tests/snapshot/t40_after_snapshot_add_learner_and_request_a_log.rs
@@ -11,13 +11,7 @@ use openraft::SnapshotPolicy;
 use crate::fixtures::init_default_ut_tracing;
 use crate::fixtures::RaftRouter;
 
-/// Test membership info is sync correctly along with snapshot.
-///
-/// What does this test do?
-///
-/// - build a stable single node cluster.
-/// - send enough requests to the node that log compaction will be triggered.
-/// - ensure that snapshot overrides the existent membership on the learner.
+/// Test log replication after snapshot replication.
 #[async_entry::test(worker_threads = 8, init = "init_default_ut_tracing()", tracing_span = "debug")]
 async fn after_snapshot_add_learner_and_request_a_log() -> Result<()> {
     let snapshot_threshold: u64 = 10;
@@ -25,7 +19,7 @@ async fn after_snapshot_add_learner_and_request_a_log() -> Result<()> {
     let config = Arc::new(
         Config {
             snapshot_policy: SnapshotPolicy::LogsSinceLast(snapshot_threshold),
-            max_applied_log_to_keep: 0,
+            max_applied_log_to_keep: 1, // not 0: do not let add-learner log to trigger a snapshot.
             purge_batch_size: 1,
             ..Default::default()
         }
@@ -34,11 +28,13 @@ async fn after_snapshot_add_learner_and_request_a_log() -> Result<()> {
     let mut router = RaftRouter::new(config.clone());
 
     let mut log_index = router.new_nodes_from_single(btreeset! {0}, btreeset! {}).await?;
+    let snapshot_index;
 
     tracing::info!("--- send just enough logs to trigger snapshot");
     {
         router.client_request_many(0, "0", (snapshot_threshold - 1 - log_index) as usize).await?;
         log_index = snapshot_threshold - 1;
+        snapshot_index = log_index;
 
         router
             .wait_for_log(
@@ -51,13 +47,13 @@ async fn after_snapshot_add_learner_and_request_a_log() -> Result<()> {
         router.assert_stable_cluster(Some(1), Some(log_index));
 
         router
-            .wait_for_snapshot(
-                &btreeset![0],
-                LogId::new(LeaderId::new(1, 0), log_index),
-                timeout(),
-                "snapshot",
+            .wait(&0, timeout())
+            .snapshot(
+                LogId::new(LeaderId::new(1, 0), snapshot_index),
+                "leader-0 has built snapshot",
             )
             .await?;
+
         router
             .assert_storage_state(
                 1,
@@ -69,43 +65,50 @@ async fn after_snapshot_add_learner_and_request_a_log() -> Result<()> {
             .await?;
     }
 
-    tracing::info!("--- create learner");
+    tracing::info!("--- add learner to the cluster to receive snapshot, which overrides the learner storage");
     {
-        tracing::info!("--- create learner");
+        router.new_raft_node(1);
+        router.add_learner(0, 1).await.expect("failed to add new node as learner");
+        log_index += 1;
 
-        tracing::info!("--- add learner to the cluster to receive snapshot, which overrides the learner storage");
-        {
-            router.new_raft_node(1);
-            router.add_learner(0, 1).await.expect("failed to add new node as learner");
-            log_index += 1;
+        tracing::info!("--- DONE add learner");
 
-            tracing::info!("--- DONE add learner");
-            router.client_request_many(0, "0", 1).await?;
-            log_index += 1;
-            tracing::info!("--- after request a log");
+        router
+            .wait(&1, timeout())
+            .snapshot(
+                LogId::new(LeaderId::new(1, 0), snapshot_index),
+                "learner-1 receives snapshot",
+            )
+            .await?;
 
-            router.wait_for_log(&btreeset![0, 1], Some(log_index), timeout(), "add learner").await?;
-            router
-                .wait_for_snapshot(&btreeset![1], LogId::new(LeaderId::new(1, 0), log_index), timeout(), "")
-                .await?;
+        log_index += router.client_request_many(0, "0", 1).await?;
+        tracing::info!("--- after request a log");
 
-            let expected_snap = Some((log_index.into(), 1));
+        router
+            .wait_for_log(
+                &btreeset![0, 1],
+                Some(log_index),
+                timeout(),
+                "learner-1 receives client-write log",
+            )
+            .await?;
 
-            router
-                .assert_storage_state(
-                    1,
-                    log_index,
-                    None, /* learner does not vote */
-                    LogId::new(LeaderId::new(1, 0), log_index),
-                    expected_snap,
-                )
-                .await?;
-        }
+        let expected_snap = Some((snapshot_index.into(), 1));
+
+        router
+            .assert_storage_state(
+                1,
+                log_index,
+                None, /* learner does not vote */
+                LogId::new(LeaderId::new(1, 0), log_index),
+                expected_snap,
+            )
+            .await?;
     }
 
     Ok(())
 }
 
 fn timeout() -> Option<Duration> {
-    Some(Duration::from_millis(5000))
+    Some(Duration::from_millis(2000))
 }
