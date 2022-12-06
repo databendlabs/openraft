be22f86eae7d3ccf264a3618a005b32e2d3c85f7
张炎泼 <drdr.xp@gmail.com>
2022-12-06 11:41:28 +0800

    Refactor: ProgressEntry: support updating matching and conflict
    Provide methods to update a matching log id or conflicting log index that
    is found on the replication target node.

--- openraft/src/progress/entry.rs
+++ openraft/src/progress/entry.rs
@@ -1,5 +1,6 @@
 use std::borrow::Borrow;
 
+use crate::summary::MessageSummary;
 use crate::LogId;
 use crate::LogIdOptionExt;
 use crate::NodeId;
@@ -33,23 +34,81 @@ impl<NID: NodeId> ProgressEntry<NID> {
             searching: None,
         }
     }
+
+    /// Create a progress entry that does not have any matching log id.
+    ///
+    /// It's going to initiate a binary search to find the minimal matching log id.
     pub(crate) fn empty(end: u64) -> Self {
+        let searching = if end == 0 {
+            None
+        } else {
+            Some(Searching {
+                mid: Self::calc_mid(0, end),
+                end,
+            })
+        };
+
         Self {
             matching: None,
-            searching: Some(Searching { mid: end / 2, end }),
+            searching,
         }
     }
 
     pub(crate) fn update_matching(&mut self, matching: Option<LogId<NID>>) {
+        tracing::debug!(
+            self = debug(&self),
+            matching = display(matching.summary()),
+            "update_matching"
+        );
+
+        debug_assert!(matching >= self.matching);
+
         self.matching = matching;
 
-        if let Some(s) = &self.searching {
-            if matching.next_index() >= s.end {
+        if let Some(s) = &mut self.searching {
+            let next = matching.next_index();
+
+            if next >= s.end {
                 self.searching = None;
             } else {
-
-                // TODO: update mid
+                s.mid = Self::calc_mid(next, s.end);
             }
+        }
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn update_conflicting(&mut self, conflict: u64) {
+        tracing::debug!(self = debug(&self), conflict = display(conflict), "update_conflict");
+
+        let matching_next = self.matching.next_index();
+
+        debug_assert!(conflict >= matching_next);
+
+        if let Some(s) = &mut self.searching {
+            debug_assert!(conflict < s.end);
+            debug_assert!(conflict >= s.mid);
+
+            s.end = conflict;
+
+            if matching_next >= s.end {
+                self.searching = None;
+            } else {
+                s.mid = Self::calc_mid(matching_next, s.end);
+            }
+        } else {
+            unreachable!("found conflict({}) when searching is None", conflict);
+        }
+    }
+
+    /// Return the starting log index range(`[start,end)`) for the next AppendEntries.
+    #[allow(dead_code)]
+    pub(crate) fn sending_start(&self) -> (u64, u64) {
+        match self.searching {
+            None => {
+                let next = self.matching.next_index();
+                (next, next)
+            }
+            Some(s) => (s.mid, s.end),
         }
     }
 
@@ -64,10 +123,90 @@ impl<NID: NodeId> ProgressEntry<NID> {
             self.matching.index()
         }
     }
+
+    fn calc_mid(matching_next: u64, end: u64) -> u64 {
+        debug_assert!(matching_next < end);
+        let d = end - matching_next;
+        let offset = d / 16 * 8;
+        matching_next + offset
+    }
 }
 
 impl<NID: NodeId> Borrow<Option<LogId<NID>>> for ProgressEntry<NID> {
     fn borrow(&self) -> &Option<LogId<NID>> {
         &self.matching
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::borrow::Borrow;
+
+    use crate::progress::entry::ProgressEntry;
+    use crate::LeaderId;
+    use crate::LogId;
+
+    fn log_id(index: u64) -> LogId<u64> {
+        LogId {
+            leader_id: LeaderId { term: 1, node_id: 1 },
+            index,
+        }
+    }
+
+    #[test]
+    fn test_update_matching() -> anyhow::Result<()> {
+        let mut pe = ProgressEntry::empty(20);
+        assert_eq!(&None, pe.borrow());
+        assert_eq!((8, 20), pe.sending_start());
+
+        pe.update_matching(None);
+        assert_eq!(&None, pe.borrow());
+        assert_eq!((8, 20), pe.sending_start());
+
+        pe.update_matching(Some(log_id(0)));
+        assert_eq!(&Some(log_id(0)), pe.borrow());
+        assert_eq!((9, 20), pe.sending_start());
+
+        pe.update_matching(Some(log_id(0)));
+        assert_eq!(&Some(log_id(0)), pe.borrow());
+        assert_eq!((9, 20), pe.sending_start());
+
+        pe.update_matching(Some(log_id(1)));
+        assert_eq!(&Some(log_id(1)), pe.borrow());
+        assert_eq!((10, 20), pe.sending_start());
+
+        pe.update_matching(Some(log_id(4)));
+        assert_eq!(&Some(log_id(4)), pe.borrow());
+        assert_eq!((5, 20), pe.sending_start());
+
+        // All logs are matching
+        pe.update_matching(Some(log_id(19)));
+        assert_eq!(&Some(log_id(19)), pe.borrow());
+        assert_eq!((20, 20), pe.sending_start());
+
+        pe.update_matching(Some(log_id(20)));
+        assert_eq!(&Some(log_id(20)), pe.borrow());
+        assert_eq!((21, 21), pe.sending_start());
+        Ok(())
+    }
+
+    #[test]
+    fn test_update_conflict() -> anyhow::Result<()> {
+        let mut pe = ProgressEntry::empty(20);
+
+        pe.update_matching(Some(log_id(4)));
+        assert_eq!(&Some(log_id(4)), pe.borrow());
+        assert_eq!((5, 20), pe.sending_start());
+
+        pe.update_conflicting(19);
+        assert_eq!(&Some(log_id(4)), pe.borrow());
+        assert_eq!((5, 19), pe.sending_start());
+
+        pe.update_conflicting(5);
+        assert_eq!(&Some(log_id(4)), pe.borrow());
+        assert_eq!((5, 5), pe.sending_start());
+        assert!(pe.searching.is_none());
+
+        Ok(())
     }
 }
