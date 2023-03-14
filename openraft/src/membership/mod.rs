ffa876c5cafc96eb7a2fa38d32bf114a4ba2d24a
张炎泼 <drdr.xp@gmail.com>
2023-03-14 11:50:19 +0800

    Refactor: move MembershipState to raft_state/
    Because `MembershipState` is a sub state of `RaftState`.
    
    And also move ChangeHandler to raft_state/membership_state; Move tests out of mod file.

--- openraft/src/membership/mod.rs
+++ openraft/src/membership/mod.rs
@@ -1,8 +1,6 @@
-mod change_handler;
 mod effective_membership;
 mod into_nodes;
 #[allow(clippy::module_inception)] mod membership;
-mod membership_state;
 mod stored_membership;
 
 #[cfg(feature = "bench")]
@@ -10,12 +8,9 @@ mod stored_membership;
 mod bench;
 
 #[cfg(test)] mod effective_membership_test;
-#[cfg(test)] mod membership_state_test;
 #[cfg(test)] mod membership_test;
 
-pub(crate) use change_handler::ChangeHandler;
 pub use effective_membership::EffectiveMembership;
 pub use into_nodes::IntoNodes;
 pub use membership::Membership;
-pub use membership_state::MembershipState;
 pub use stored_membership::StoredMembership;
---
+++ openraft/src/raft_state/membership_state/change_handler.rs
@@ -0,0 +1,65 @@
+use crate::error::ChangeMembershipError;
+use crate::error::InProgress;
+use crate::ChangeMembers;
+use crate::Membership;
+use crate::MembershipState;
+use crate::Node;
+use crate::NodeId;
+
+/// This struct handles change-membership requests, validating them and applying the changes if
+/// the necessary conditions are met. It operates at the `Engine` and `RaftState` level, and
+/// serves as the outermost API for a consensus engine.
+pub(crate) struct ChangeHandler<'m, NID, N>
+where
+    NID: NodeId,
+    N: Node,
+{
+    pub(crate) state: &'m MembershipState<NID, N>,
+}
+
+impl<'m, NID, N> ChangeHandler<'m, NID, N>
+where
+    NID: NodeId,
+    N: Node,
+{
+    /// Builds a new membership configuration by applying changes to the current configuration.
+    ///
+    /// * `changes`: The changes to apply to the current membership configuration.
+    /// * `retain` specifies whether to retain the removed voters as a learners, i.e., nodes that
+    ///   continue to receive log replication from the leader.
+    ///
+    /// A Result containing the new membership configuration if the operation succeeds, or a
+    /// `ChangeMembershipError` if an error occurs.
+    ///
+    /// This function ensures that the cluster will have at least one voter in the new membership
+    /// configuration.
+    pub(crate) fn apply(
+        &self,
+        change: ChangeMembers<NID, N>,
+        retain: bool,
+    ) -> Result<Membership<NID, N>, ChangeMembershipError<NID>> {
+        self.ensure_committed()?;
+
+        let new_membership = self.state.effective().membership().clone().change(change, retain)?;
+        Ok(new_membership)
+    }
+
+    /// Ensures that the latest membership has been committed.
+    ///
+    /// Returns Ok if the last membership is committed, or an InProgress error
+    /// otherwise, to indicate a change-membership request should be rejected.
+    pub(crate) fn ensure_committed(&self) -> Result<(), InProgress<NID>> {
+        let effective = self.state.effective();
+        let committed = self.state.committed();
+
+        if effective.log_id() == committed.log_id() {
+            // Ok: last membership(effective) is committed
+            Ok(())
+        } else {
+            Err(InProgress {
+                committed: *committed.log_id(),
+                membership_log_id: *effective.log_id(),
+            })
+        }
+    }
+}
