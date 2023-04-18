113322148c30dfef79ce8e78c2938dea54a1f2db
张炎泼 <drdr.xp@gmail.com>
2023-04-18 14:38:06 +0800

    Test: test building snapshot does not block append-entries
    Refactor: refine integration tests names

--- tests/tests/life_cycle/main.rs
+++ tests/tests/life_cycle/main.rs
@@ -8,8 +8,8 @@ mod fixtures;
 // The number indicate the preferred running order for these case.
 // The later tests may depend on the earlier ones.
 
-mod t20_initialization;
-mod t20_shutdown;
-mod t30_follower_restart_does_not_interrupt;
-mod t30_single_follower_restart;
+mod t10_initialization;
+mod t11_shutdown;
+mod t50_follower_restart_does_not_interrupt;
+mod t50_single_follower_restart;
 mod t90_issue_607_single_restart;
--- tests/tests/snapshot/main.rs
+++ tests/tests/snapshot/main.rs
@@ -5,15 +5,15 @@
 #[path = "../fixtures/mod.rs"]
 mod fixtures;
 
-mod t20_api_install_snapshot;
+mod t10_api_install_snapshot;
 mod t20_startup_snapshot;
 mod t20_trigger_snapshot;
-mod t23_snapshot_chunk_size;
-mod t24_snapshot_when_lacking_log;
-mod t25_snapshot_line_rate_to_snapshot;
-mod t40_after_snapshot_add_learner_and_request_a_log;
-mod t40_purge_in_snapshot_logs;
-mod t41_snapshot_overrides_membership;
-mod t42_snapshot_uses_prev_snap_membership;
-mod t43_snapshot_delete_conflict_logs;
-mod t44_replication_does_not_block_purge;
+mod t30_purge_in_snapshot_logs;
+mod t31_snapshot_overrides_membership;
+mod t32_snapshot_uses_prev_snap_membership;
+mod t33_snapshot_delete_conflict_logs;
+mod t34_replication_does_not_block_purge;
+mod t50_snapshot_line_rate_to_snapshot;
+mod t50_snapshot_when_lacking_log;
+mod t51_after_snapshot_add_learner_and_request_a_log;
+mod t60_snapshot_chunk_size;
