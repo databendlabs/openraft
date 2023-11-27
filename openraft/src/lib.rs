70a2ff1c0c64eedf2a12226b6ea910c2b39270bb
张炎泼 <drdr.xp@gmail.com>
2023-11-27 13:49:54 +0800

    Dep: update toolchain

--- openraft/src/lib.rs
+++ openraft/src/lib.rs
@@ -1,6 +1,5 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(feature = "bt", feature(error_generic_member_access))]
-#![cfg_attr(feature = "bt", feature(provide_any))]
 #![cfg_attr(feature = "bench", feature(test))]
 // TODO: `clippy::result-large-err`: StorageError is 136 bytes, try to reduce the size.
 #![allow(clippy::bool_assert_comparison, clippy::type_complexity, clippy::result_large_err)]
