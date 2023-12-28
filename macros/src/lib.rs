12eda69531cc6c58f2920daddc5ee931124af5b0
Changgyoo Park <changgyoo.park@sap.com>
2023-12-28 12:58:00 +0900

    Fix: lint errors

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,11 +2,11 @@ use proc_macro::TokenStream;
 
 /// This macro emits `#[async_trait::async_trait]` if the `singlethreaded` feature is disabled.
 ///
-/// Starting from Rust 1.75.0, the `async_fn_in_trait` language feature is generally available 
-/// that allows traits to contain asynchronous methods and associated functions. However, the 
+/// Starting from Rust 1.75.0, the `async_fn_in_trait` language feature is generally available
+/// that allows traits to contain asynchronous methods and associated functions. However, the
 /// feature has several known issues that are mainly related to Rust compiler's abilities to infer
 /// `Send` bounds of associated types: [`90696``](https://github.com/rust-lang/rust/issues/90696).
-/// 
+///
 /// Therefore, if a trait requires `Send` bounds in its associated data types, this macro
 /// circumvents the compiler shortcomings by using the
 /// [`async-trait`](https://crates.io/crates/async-trait) crate which boxes return
