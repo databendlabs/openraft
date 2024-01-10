936e70bfe04785801adcfbd77c20bcc15bb81bb1
张炎泼 <drdr.xp@gmail.com>
2024-01-10 19:41:33 +0800

    Test: add example of singlethreaded kv-store
    This example `examples/raft-kv-memstore-singlethreaded` illustrates
    building a single threaded Openraft application.
    
    By enabling feature flag `singlethreaded`, Openraft does not require the
    data types to be `Send`, which enables using `Rc` in place of `Arc` to
    gain a performance improvement.

---
+++ examples/raft-kv-memstore-singlethreaded/src/network.rs
@@ -0,0 +1,72 @@
+use openraft::add_async_trait;
+use openraft::error::InstallSnapshotError;
+use openraft::error::RemoteError;
+use openraft::raft::AppendEntriesRequest;
+use openraft::raft::AppendEntriesResponse;
+use openraft::raft::InstallSnapshotRequest;
+use openraft::raft::InstallSnapshotResponse;
+use openraft::raft::VoteRequest;
+use openraft::raft::VoteResponse;
+use openraft::RaftNetwork;
+use openraft::RaftNetworkFactory;
+
+use crate::router::Router;
+use crate::typ;
+use crate::BasicNode;
+use crate::NodeId;
+use crate::TypeConfig;
+
+pub struct Connection {
+    router: Router,
+    target: NodeId,
+}
+
+// NOTE: This could be implemented also on `Arc<ExampleNetwork>`, but since it's empty, implemented
+// directly.
+#[add_async_trait]
+impl RaftNetworkFactory<TypeConfig> for Router {
+    type Network = Connection;
+
+    async fn new_client(&mut self, target: NodeId, _node: &BasicNode) -> Self::Network {
+        Connection {
+            router: self.clone(),
+            target,
+        }
+    }
+}
+
+#[add_async_trait]
+impl RaftNetwork<TypeConfig> for Connection {
+    async fn send_append_entries(
+        &mut self,
+        req: AppendEntriesRequest<TypeConfig>,
+    ) -> Result<AppendEntriesResponse<NodeId>, typ::RPCError> {
+        let resp = self
+            .router
+            .send(self.target, "/raft/append", req)
+            .await
+            .map_err(|e| RemoteError::new(self.target, e))?;
+        Ok(resp)
+    }
+
+    async fn send_install_snapshot(
+        &mut self,
+        req: InstallSnapshotRequest<TypeConfig>,
+    ) -> Result<InstallSnapshotResponse<NodeId>, typ::RPCError<InstallSnapshotError>> {
+        let resp = self
+            .router
+            .send(self.target, "/raft/snapshot", req)
+            .await
+            .map_err(|e| RemoteError::new(self.target, e))?;
+        Ok(resp)
+    }
+
+    async fn send_vote(&mut self, req: VoteRequest<NodeId>) -> Result<VoteResponse<NodeId>, typ::RPCError> {
+        let resp = self
+            .router
+            .send(self.target, "/raft/vote", req)
+            .await
+            .map_err(|e| RemoteError::new(self.target, e))?;
+        Ok(resp)
+    }
+}
