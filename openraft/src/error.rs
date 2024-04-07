f5cc981ec1157b764442affb120854b284549dad
Your Name <you@example.com>
2024-04-07 22:56:42 +0200

    Feature: implement TryAsRef<ForwardToLeader> for RaftError

--- openraft/src/error.rs
+++ openraft/src/error.rs
@@ -107,22 +107,6 @@ where
     }
 }
 
-/*
-for 0.9 versions
-
-impl<NID, N , E> TryAsRef<ForwardToLeader<NID, N>> for RaftError<NID, E>
-where
-    NID: NodeId,
-    N: Node,
-    E: Debug + TryAsRef<ForwardToLeader<NID, N>>,
-{
-    fn try_as_ref(&self) -> Option<&ForwardToLeader<NID, N>> {
-        self.forward_to_leader()
-    }
-}
-
-*/
-
 impl<C, E> From<StorageError<C::NodeId>> for RaftError<C, E>
 where C: RaftTypeConfig
 {
