dc9637e208352a38f7d15e84b8f311391e05f34a
Steve Lau <stevelauc@outlook.com>
2024-07-31 18:48:07 +0800

    Refactor: remove OptionalSync bound from Guard

--- openraft/src/type_config/async_runtime/mutex.rs
+++ openraft/src/type_config/async_runtime/mutex.rs
@@ -7,7 +7,7 @@ use crate::OptionalSync;
 /// Represents an implementation of an asynchronous Mutex.
 pub trait Mutex<T: OptionalSend + 'static>: OptionalSend + OptionalSync {
     /// Handle to an acquired lock, should release it when dropped.
-    type Guard<'a>: DerefMut<Target = T> + OptionalSend + OptionalSync
+    type Guard<'a>: DerefMut<Target = T> + OptionalSend
     where Self: 'a;
 
     /// Creates a new lock.
