4f26fcb1c55c157215da38a68da7ae8342e7fee0
张炎泼 <drdr.xp@gmail.com>
2025-01-25 11:22:23 +0800

    Make RaftLogId private

--- openraft/src/raft_state/log_state_reader.rs
+++ openraft/src/raft_state/log_state_reader.rs
@@ -1,9 +1,9 @@
 use crate::log_id::option_raft_log_id_ext::OptionRaftLogIdExt;
 use crate::log_id::option_ref_log_id_ext::OptionRefLogIdExt;
+use crate::log_id::raft_log_id::RaftLogId;
 use crate::log_id::raft_log_id_ext::RaftLogIdExt;
 use crate::log_id::ref_log_id::RefLogId;
 use crate::type_config::alias::LogIdOf;
-use crate::RaftLogId;
 use crate::RaftTypeConfig;
 
 /// APIs to get significant log ids reflecting the raft state.
