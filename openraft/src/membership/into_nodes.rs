a529931e633e641cac09d078ed815a0d7c15a3c3
Zhang Yanpo <drdr.xp@gmail.com>
2025-03-16 22:42:10 +0800

    feat: Add impl `IntoNodes<NID, EmptyNode>` for `BTreeSet<NID>`
    EmptyNode can be created directly thus a BTreeSet can be used to build a
    nodes map

--- openraft/src/membership/into_nodes.rs
+++ openraft/src/membership/into_nodes.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 
 use maplit::btreemap;
 
+use crate::EmptyNode;
 use crate::Node;
 use crate::NodeId;
 
@@ -34,6 +35,14 @@ where NID: NodeId
 {
     fn into_nodes(self) -> BTreeMap<NID, ()> {
         self.into_iter().map(|node_id| (node_id, ())).collect()
+    }
+}
+
+impl<NID> IntoNodes<NID, EmptyNode> for BTreeSet<NID>
+where NID: NodeId
+{
+    fn into_nodes(self) -> BTreeMap<NID, EmptyNode> {
+        self.into_iter().map(|node_id| (node_id, EmptyNode {})).collect()
     }
 }
 
