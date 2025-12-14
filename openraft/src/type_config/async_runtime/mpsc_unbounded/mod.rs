9d3369967491cb22d7aae1a4e8c09af9659ba93f
Zhang Yanpo <drdr.xp@gmail.com>
2025-12-14 16:20:28 +0800

    refactor: add `#[track_caller]` to async runtime and time trait methods
    Add `#[track_caller]` attribute to trait method definitions so all
    implementations automatically inherit caller location tracking.
    
    Changes:
    - Add to `AsyncRuntime`: `spawn`, `sleep`, `sleep_until`, `timeout`, `timeout_at`, `is_panic`, `thread_rng`
    - Add to `Mpsc`, `MpscSender`, `MpscReceiver`, `MpscWeakSender` methods
    - Add to `MpscUnbounded`, `MpscUnboundedSender`, `MpscUnboundedReceiver`, `MpscUnboundedWeakSender` methods
    - Add to `Watch`, `WatchSender`, `WatchReceiver` methods
    - Add to `Oneshot`, `OneshotSender` methods
    - Add to `Mutex`: `new`, `lock`
    - Add to `Instant`: `now`, `elapsed`
    - Add to `TypeConfigExt` wrapper methods

--- openraft/src/type_config/async_runtime/mpsc_unbounded/mod.rs
+++ openraft/src/type_config/async_runtime/mpsc_unbounded/mod.rs
@@ -19,6 +19,7 @@ pub trait MpscUnbounded: Sized + OptionalSend {
     type WeakSender<T: OptionalSend>: MpscUnboundedWeakSender<Self, T>;
 
     /// Creates an unbounded MPSC channel for communicating between asynchronous tasks.
+    #[track_caller]
     fn channel<T: OptionalSend>() -> (Self::Sender<T>, Self::Receiver<T>);
 }
 
@@ -32,12 +33,14 @@ where
     ///
     /// If the receiving half of the channel is closed, this
     /// function returns an error. The error includes the value passed to `send`.
+    #[track_caller]
     fn send(&self, msg: T) -> Result<(), SendError<T>>;
 
     /// Converts the [`MpscUnboundedSender`] to a [`MpscUnboundedWeakSender`] that does not count
     /// towards RAII semantics, i.e., if all `Sender` instances of the
     /// channel were dropped and only `WeakSender` instances remain,
     /// the channel is closed.
+    #[track_caller]
     fn downgrade(&self) -> MU::WeakSender<T>;
 }
 
@@ -47,6 +50,7 @@ pub trait MpscUnboundedReceiver<T>: OptionalSend + OptionalSync {
     ///
     /// This method returns `None` if the channel has been closed and there are
     /// no remaining messages in the channel's buffer.
+    #[track_caller]
     fn recv(&mut self) -> impl Future<Output = Option<T>> + OptionalSend;
 
     /// Tries to receive the next value for this receiver.
@@ -56,6 +60,7 @@ pub trait MpscUnboundedReceiver<T>: OptionalSend + OptionalSync {
     ///
     /// This method returns the [`TryRecvError::Disconnected`] error if the channel is
     /// currently empty, and there are no outstanding senders.
+    #[track_caller]
     fn try_recv(&mut self) -> Result<T, TryRecvError>;
 }
 
@@ -72,5 +77,6 @@ where
     ///
     /// This will return `Some` if there are other `Sender` instances alive and
     /// the channel wasn't previously dropped, otherwise `None` is returned.
+    #[track_caller]
     fn upgrade(&self) -> Option<MU::Sender<T>>;
 }
