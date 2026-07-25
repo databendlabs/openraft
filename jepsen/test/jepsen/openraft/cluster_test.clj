d3a1f70714bd3943956c672479bb10ff0c852e34
Jiawei Zhao <Phoenix500526@163.com>
2026-07-25 14:29:51 +0800

    feat: add process nemesis
    Exercise leader and follower process failures while preserving a voting
    quorum. Share quorum selection with the partition nemesis so fault
    targets remain valid for stable and joint memberships.
    
    Signed-off-by: Jiawei Zhao <Phoenix500526@163.com>

--- jepsen/test/jepsen/openraft/cluster_test.clj
+++ jepsen/test/jepsen/openraft/cluster_test.clj
@@ -38,3 +38,22 @@
                               :current_leader nil}}]
     (with-redefs [client/metrics! metrics]
       (is (nil? (#'cluster/cluster-status test-config))))))
+
+(defn- membership [configs]
+  {:membership_config
+   {:membership
+    {:configs configs}}})
+
+(deftest maps-voter-configs-to-jepsen-nodes
+  (let [status {:leader "n2"
+                :metrics {"n2" (membership [[1 2 3]])}}]
+    (is (= [#{"n1" "n2" "n3"}]
+           (cluster/voter-configs test-config status)))))
+
+(deftest maps-joint-voter-configs
+  (let [test (assoc test-config :nodes ["n1" "n2" "n3" "n4"])
+        status {:leader "n2"
+                :metrics {"n2" (membership [[1 2 3] [1 2 4]])}}]
+    (is (= [#{"n1" "n2" "n3"}
+            #{"n1" "n2" "n4"}]
+           (cluster/voter-configs test status)))))
