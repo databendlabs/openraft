8d5b05d9bd57cca54782bad0494be591a12ff1b5
Jiawei Zhao <Phoenix500526@163.com>
2026-08-08 09:59:13 +0800

    test: jepsen: fix exclude socket timeouts
    SocketTimeoutException inherits InterruptedIOException.
    It represents a transport timeout, not a thread interruption.
    Excluding it prevents future SSH timeout configuration from aborting
    Jepsen runs.
    
    Refs #1974
    
    Signed-off-by: Jiawei Zhao <Phoenix500526@163.com>

--- jepsen/src/jepsen/openraft/interruption.clj
+++ jepsen/src/jepsen/openraft/interruption.clj
@@ -1,9 +1,11 @@
 (ns jepsen.openraft.interruption)
 
 (defn interruption?
-  "True when an exception signals that the current thread was interrupted."
+  "True when e signals that the current thread was interrupted, in any of the
+  forms the HTTP client and jepsen.control.sshj produce."
   [e]
   (or (instance? InterruptedException e)
-      (instance? java.io.InterruptedIOException e)
+      (and (instance? java.io.InterruptedIOException e)
+           (not (instance? java.net.SocketTimeoutException e)))
       (instance? java.nio.channels.ClosedByInterruptException e)
       (= :interrupted (:kind (ex-data e)))))
