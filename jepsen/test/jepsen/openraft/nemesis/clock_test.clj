9ef378a8be3c212d09991bac55f15c4e8105ea1f
Jiawei Zhao <Phoenix500526@163.com>
2026-08-25 13:45:17 +0800

    test: jepsen: add wall-clock bump faults
    Forward and backward clock jumps can expose unsafe dependencies on wall time.
    Multi-scale offsets and unconstrained target subsets exercise both local skew
    and loss-of-quorum scenarios while preserving complete history evidence.
    
    Refs #2041
    
    Signed-off-by: Jiawei Zhao <Phoenix500526@163.com>

---
+++ jepsen/test/jepsen/openraft/nemesis/clock_test.clj
@@ -0,0 +1,50 @@
+(ns jepsen.openraft.nemesis.clock-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [jepsen [checker :as checker]
+             [control :as c]
+             [nemesis :as nemesis]]
+            [jepsen.openraft.clock :as clock]
+            [jepsen.openraft.cluster :as cluster]
+            [jepsen.openraft.nemesis.clock :as clock-nemesis]))
+
+(def test-config {:nodes ["n1" "n2" "n3" "n4" "n5"]})
+
+(deftest bump-installs-node-local-offsets
+  (let [settings (atom {})
+        subject (clock-nemesis/clock-nemesis)]
+    (with-redefs [c/on-nodes (fn [_test nodes f]
+                               (into {} (map (fn [node] [node (f test-config node)])) nodes))
+                  clock/verify-application-clock! (constantly "loaded")
+                  clock/write-setting! (fn [setting]
+                                         (swap! settings assoc :current setting)
+                                         setting)
+                  clock-nemesis/verify-offset!
+                  (fn [offset-ms]
+                    {:setting (get @settings :current)
+                     :observed-offset-ms offset-ms})
+                  cluster/membership-status (constantly {:leader "n1"})]
+      (let [subject (nemesis/setup! subject test-config)
+            result (nemesis/invoke! subject
+                                    test-config
+                                    {:type :info
+                                     :f :bump-clock
+                                     :value {"n1" 1000 "n2" -2000}})]
+        (is (= :installed (get-in result [:value :status])))
+        (is (= :bump (get-in result [:value :mode])))
+        (is (= true (get-in result [:value :leader-included])))
+        (is (= #{"n1" "n2"} (set (get-in result [:value :targets]))))))))
+
+(deftest clock-checker-requires-bump-and-final-reset
+  (let [subject (:checker (clock-nemesis/clock-package))
+        installed (fn [f] {:type :info :f f :value {:status :installed}})]
+    (testing "complete coverage"
+      (is (true? (:valid? (checker/check subject
+                                         test-config
+                                         [(installed :bump-clock)
+                                          (installed :reset-clock)]
+                                         {})))))
+    (testing "missing bump"
+      (is (false? (:valid? (checker/check subject
+                                          test-config
+                                          [(installed :reset-clock)]
+                                          {})))))))
